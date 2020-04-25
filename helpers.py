e9ba6302aa4047bd976c4448e46180b162b776a8
Tran Sam <quocsam93@gmail.com>
2020-04-25 16:45:09 -0400

    formulating pipeline to preprocess data

---
+++ helpers.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        ax.imshow(img)
+    plt.tight_layout()
+    plt.show()
