8ddfcc0e600f7512bd1f5cec99b61ffa6396df4f
Tran Sam <quocsam93@gmail.com>
2020-04-25 17:41:15 -0400

    working data image generator

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+def plotImages(images_arr, number_images=5, img_size=20):
+    fig, axes = plt.subplots(1, number_images, figsize=(img_size, img_size))
     axes = axes.flatten()
     for img, ax in zip( images_arr, axes):
         ax.imshow(img)
