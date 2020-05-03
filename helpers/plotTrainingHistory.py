931cf02d14ce91a996637053353011ec5bf95b22
Tran Sam <quocsam93@gmail.com>
2020-05-03 19:01:40 -0400

    best simple model and its history, helpers functions to plot the training history, callbacks functions incorporated

---
+++ helpers/plotTrainingHistory.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+def plotTrainingHistory(history):
+    plt.plot(history['accuracy'])
+    plt.plot(history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
